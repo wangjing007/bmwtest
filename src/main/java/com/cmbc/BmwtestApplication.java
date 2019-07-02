bd0612ecfe47069da60de4347251f15b8506f85f
liwenbiao <liwenbiao@baoxinkj.com>
2019-07-02 14:50:03 +0800

    first

---
+++ src/main/java/com/cmbc/BmwtestApplication.java
@@ -0,0 +1,13 @@
+package com.cmbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmwtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmwtestApplication.class, args);
+    }
+
+}
